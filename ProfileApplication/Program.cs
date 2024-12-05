115ab5e96c0966d685b5d8a4b6399b8ccdad7012
artyomte <53403320+Wanderer76@users.noreply.github.com>
2024-12-05 10:36:01 +0500

    Замена UseEndpoints на MapControllers

--- ProfileApplication/Program.cs
+++ ProfileApplication/Program.cs
@@ -4,8 +4,6 @@ using Profile.Service.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
@@ -13,12 +11,10 @@ builder.Services.AddProfileServices();
 builder.Services.AddPersistence(builder.Configuration);
 builder.Services.AddCustomJwtAuthentication();
 builder.Services.AddAuthorization();
-builder.Services.AddCors(x=>
-x.AddDefaultPolicy(b=>b.AllowAnyMethod().AllowAnyOrigin()));
+builder.Services.AddCors(x => x.AddDefaultPolicy(b => b.AllowAnyMethod().AllowAnyOrigin()));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -30,10 +26,7 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers(); // подключаем маршрутизацию на контроллеры
-});
+app.MapControllers();
 
 
 app.Run();
