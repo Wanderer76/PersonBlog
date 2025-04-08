e42b6ce985e96ecb3f57215b385b9f89010c6a26
artyomte <53403320+Wanderer76@users.noreply.github.com>
2025-04-08 10:51:38 +0500

    Добавил объекты для result-pattern

---
+++ Shared/Utils/Error.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Utils
+{
+    public record Error(string Code, string? Message = null)
+    {
+        public ErrorList ToErrorList() => new([this]);
+    }
+
+}
---
+++ Shared/Utils/Result.cs
@@ -0,0 +1,39 @@
+﻿namespace Shared.Utils
+{
+    public sealed class Result<TValue, TError> where TError : class
+    {
+        private readonly TValue? _value;
+        public TError? Error { get; }
+        public bool IsSuccess { get; }
+
+        private Result(TValue value)
+        {
+            Value = value;
+            IsSuccess = true;
+            Error = null;
+        }
+        private Result(TError error)
+        {
+            IsSuccess = false;
+            Error = error ?? throw new ArgumentException("invalid error", nameof(error));
+        }
+
+        public TValue Value
+        {
+            get
+            {
+                if (IsFailure)
+                {
+                    throw new InvalidOperationException("there is no value for failure");
+                }
+                return _value!;
+            }
+            private init => _value = value;
+        }
+
+        public bool IsFailure => !IsSuccess;
+
+        public static Result<TValue, TError> Success(TValue value) => new(value);
+        public static Result<TValue, TError> Failure(TError error) => new(error);
+    }
+}
