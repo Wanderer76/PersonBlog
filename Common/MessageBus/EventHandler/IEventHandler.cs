ff58355cb7c47a5dcd4de4955e3d492d80da1865
Wanderer76 <ateplinsky@mail.ru>
2025-05-23 14:59:00 +0500

    Правка

--- Common/MessageBus/EventHandler/IEventHandler.cs
+++ Common/MessageBus/EventHandler/IEventHandler.cs
@@ -1,26 +1,29 @@
-﻿
-namespace MessageBus.EventHandler
+﻿namespace MessageBus.EventHandler;
+
+public interface IEventHandler
 {
-    public interface IEventHandler
+    Task Handle(MessageContext<object> @event);
+}
+
+public interface IEventHandler<TEvent> : IEventHandler
+{
+    Task Handle(MessageContext<TEvent> @event);
+    Task IEventHandler.Handle(MessageContext<object> @event) => Handle(@event);
+}
+
+public sealed class MessageContext<TMessage>
+{
+    public Guid? CorrelationId { get; set; }
+    public TMessage Message { get; set; }
+
+    public MessageContext(Guid? correlationId, TMessage message)
     {
-        Task Handle(MessageContext<object> @event);
+        CorrelationId = correlationId;
+        Message = message;
     }
 
-    public interface IEventHandler<TEvent> : IEventHandler
+    public static implicit operator MessageContext<TMessage>(MessageContext<object> t)
     {
-        Task Handle(MessageContext<TEvent> @event);
-        Task IEventHandler.Handle(MessageContext<object> @event) => Handle(new MessageContext<TEvent>(@event.CorrelationId, (TEvent)@event.Message));
-    }
-
-    public sealed class MessageContext<TMessage>
-    {
-        public Guid? CorrelationId { get; set; }
-        public TMessage Message { get; set; }
-
-        public MessageContext(Guid? correlationId, TMessage message)
-        {
-            CorrelationId = correlationId;
-            Message = message;
-        }
+        return new(t.CorrelationId, (TMessage)t.Message);
     }
 }
