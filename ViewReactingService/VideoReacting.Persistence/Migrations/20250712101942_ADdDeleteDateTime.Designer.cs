f50c3a390436d10bd7fe9a17986e241c2466e988
Wanderer76 <ateplinsky@mail.ru>
2025-07-12 15:21:43 +0500

    Добавил недостающую миграцию

--- ViewReactingService/VideoReacting.Persistence/Migrations/20250712101942_ADdDeleteDateTime.Designer.cs
+++ ViewReactingService/VideoReacting.Persistence/Migrations/20250712101942_ADdDeleteDateTime.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using VideoReacting.Persistence;
@@ -11,9 +12,11 @@ using VideoReacting.Persistence;
 namespace VideoReacting.Persistence.Migrations
 {
     [DbContext(typeof(ViewReactingDbContext))]
-    partial class ViewReactingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250712101942_ADdDeleteDateTime")]
+    partial class ADdDeleteDateTime
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
